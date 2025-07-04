13f86808a9a2be488d6a01f7859c8b8954ec7142
Evelyn Haydee <84814742+eve025@users.noreply.github.com>
2025-07-03 19:39:10 -0600

    Falta la verificacion de input horas que no este vacio(para evitar errores en la tabla), mostrar los elementos descriptivos con la tabla y datos ingresados

--- js/vidaMedia.js
+++ js/vidaMedia.js
@@ -12,19 +12,41 @@ function crearTabla(e){
 
     const inputConcentracion = document.querySelector("#concentracion");
     
-    concentracion = inputConcentracion.value;
+    let concentracion = inputConcentracion.value;
     //se esconde el btn para crear
     buttonStart.style.display = "none";
 
     //deshabilitar escribir en input
     inputConcentracion.disabled = true;
+    
+    const inputHrs = document.querySelector("#horasT");
+    
+    let Hrs = inputHrs.value;
+    //deshabilitar escribir en input
+    inputHrs.disabled = true;
+
+
 
     if(concentracion.length === 0 || concentracion === ""){
         console.log("try again");
         console.log("la concentracion es " + concentracion);
+        
+        //mostrar leyenda con datos ingresados
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        h1.textContent = "La tabla no puede ser generada, ingrese datos denuevo.."
+        div.appendChild(h1);
+
     }else{
         console.log("no esta vacio");
         console.log("la concentracion es " + concentracion);
+
+        //mostrar leyenda con datos ingresados
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        h1.textContent = "La tabla generada muestra una concentracion inicial '${concentracion}' y vida media inicial: '${Hrs}' "
+        div.appendChild(h1);
+
         //NOTA: en este programa se toma un umbral para parar las divisiones
         let row = 0;
         for(let i = concentracion; i>=0.001; i = i/2){
@@ -48,36 +70,98 @@ function crearTabla(e){
                 
 
                 //posicion 0  del arreglo(fila 0), muestra la concentracion
-                if(rowi[i] == rowi[0] && celda[j] == celda[0]){
+                if(i == 0 && j == 2){
                     //agregando texto por default(concentracion)
                     celda[j].innerHTML = concentracion;
                     // celda[0].innerHTML = concentracion;
                     // console.log("0,0")
-                }else if(rowi[i] == rowi[0] && celda[1] == celda[1]){
+                }else if(i == 0 && j == 0){
                     celda[j].innerHTML = 0;
                     //agregando texto por default(0)
-                }else if(rowi[i] == rowi[0] && celda[2] == celda[2]){
+                }else if(i == 0 && j == 1){
                     //agregando texto por default(0)
                     celda[j].innerHTML = 0;
                 }
 
                 //---------------------------------
-                //resultados columna 1, a partir de la fila 0 
-                 if(i === 1 && j === 0){
-                    //escribiendo el calculo d ela concentracion a la mitad
-                    celda[j].innerHTML = concentracion/2;
-                 }
-                //resultados para columna 1 y 2
-                if(i >= 1 && j >= 1){
-                    
+                //resultados para columna 0 y 1
+                //estamos en el for de la j osea columnas
+                //i=fila              j=columna 
+                let vh = [];
+                if(i >= 1 && j == 0){
+                    for(let k= 0 ; k <= row;k++){
+                        vh.push(k);
+                        // celda[j].innerHTML = calcC;
+                        // console.log(calcC)
+                    }
+                    //recorrer el arreglo
+                    for(let hora of vh){
+                        //se pasan a string para crear el elemento
+                        hora.toString();
+                        console.log(hora)
+                    }
+
+                    celda[j].innerHTML = vh[i];
                 }
+                
+
+                //resultados columna 1, a partir de la fila 0 en adelante
+                let h = [];
+                let sumatoria = 0;
+                if(i == 1 && j == 1){
+                    celda[j].innerHTML = Hrs;
+                }else if(i >= 2 && j == 1){
+                    for(let k= 1 ; k <= row;k++){
+                        h.push(sumatoria);
+                        sumatoria = sumatoria + parseInt(Hrs);
+                        
+                    }
+
+                    //recorrer horas
+                    for(let hora of h){
+                        hora.toString();
+                        console.log(hora)
+                    }
+                    celda[j].innerHTML = h[i];
+                }
+
+
+                //resultados columna 2, a partir de la fila 0 
+                let C = [];
+                let calcC = parseFloat(concentracion);
+                if(i >= 1 && j == 2){
+                    //escribiendo el calculo de la concentracion a la mitad
+                    
+                    //calcular mitades y guardar
+                    for(let k= 1 ; k <= row;k++){
+                        
+                        C.push(calcC);
+                        calcC = calcC/2;
+                        // celda[j].innerHTML = calcC;
+                        // console.log(calcC)
+                    }
+                    
+                    //recorrer el arreglo
+                    for(let cNum of C){
+                        cNum.toString();
+                        // celda[j].innerHTML = cNum.toString();
+                        console.log(cNum)
+                    }
+                    celda[j].innerHTML = C[i];
+                    // console.log(concentracion)
+                    console.log(C)
+                    
+                 }
                 // añadiendo la celda a la fila
                 rowi[i].appendChild(celda[j]);
                 
             }
             tabla.appendChild(rowi[i]);
         }
-        
-        
     }
+}
+
+//crear la grafica
+function crearCanvas(){
+
 }
